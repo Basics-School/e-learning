13445cb1985f76afa72e336f926d3083315226b3
ktiakib <ktiakib@gmail.com>
2022-07-04 21:55:03 +0600

    fuull

---
+++ client/pages/index.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import CourseCard from "../components/cards/CourseCard";
+
+const Index = ({ courses }) => {
+  // const [courses, setCourses] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchCourses = async () => {
+  //     const { data } = await axios.get("/api/courses");
+  //     setCourses(data);
+  //   };
+  //   fetchCourses();
+  // }, []);
+
+  return (
+    <>
+      <h1 className='jumbotron text-center bg-primary square'>
+        Welcome To Basic's School
+      </h1>
+      <div className='container-fluid'>
+        <div className='row'>
+          {courses.map((course) => (
+            <div key={course._id} className='col-md-4'>
+              <CourseCard course={course} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export async function getServerSideProps() {
+  const { data } = await axios.get(`${process.env.API}/courses`);
+  return {
+    props: {
+      courses: data,
+    },
+  };
+}
+
+export default Index;
